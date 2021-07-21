5f7a29ffe1651f599becfbbf4c3f850be1bbcc84
Joaquin Viretti <joaquin.viretti@gmail.com>
2021-07-21 15:05:13 -0300

    borro imports innecesarios

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,4 @@
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
 import "./ItemListContainer.scss"
 
 const ItemListContainer = ({nombre}) =>
