6c9f0b23fe0eb92e9231d32fe537650ddfd0661d
Joaquinviretti <joaquin.viretti@gmail.com>
2021-07-26 14:21:57 -0300

    agrego reset.scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+//normalizer
+import './reset.scss'
+
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 
